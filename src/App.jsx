6936f7edeeb671e7a33c1bfce0e57c03adee8673
Daigo Fujiwara <daigo@daigofujiwara.com>
2025-02-26 00:41:06 -0500

    fix center issue, font size

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { useRef, useEffect, useState } from 'react'
-import { OrbitControls, Text3D } from "@react-three/drei";
+import { OrbitControls, Text3D, Center } from "@react-three/drei";
 import { EffectComposer, Bloom } from '@react-three/postprocessing';
 import './App.css'
 import './RainbowMaterial';
@@ -9,19 +9,6 @@ function RainbowText({ text }) {
   const materialRef = useRef();
   const textRef = useRef();
   const { size } = useThree();
-  const [textSize, setTextSize] = useState(0.5);
-  const [textPosition, setTextPosition] = useState([0, 0, 0]);
-
-  // Center the text based on its size
-  useEffect(() => {
-    if (textRef.current) {
-      textRef.current.geometry.computeBoundingBox();
-      const textRefWidth = textRef.current.geometry.boundingBox.max.x - textRef.current.geometry.boundingBox.min.x;
-      const textRefHeight = textRef.current.geometry.boundingBox.max.y - textRef.current.geometry.boundingBox.min.y;
-      setTextSize(size.width * 0.0004);
-      setTextPosition([-textRefWidth / 2, -textRefHeight / 2, 0]);
-    }
-  }, [size.width]);
 
   // useFrame in R3F is a hook that runs every frame rendered, similar to a render loop.
   // You receive the state (useThree) and a clock delta https://r3f.docs.pmnd.rs/api/hooks#useframe
@@ -30,21 +17,24 @@ function RainbowText({ text }) {
       materialRef.current.time += delta * 0.3;
     }
     if (textRef.current) {
+      // textRef.current.rotation.y += delta * 0.1;
       textRef.current.position.z = Math.sin(state.clock.elapsedTime) * 0.4;
     }
   });
 
   return (
-    <Text3D
-      position={textPosition}
-      ref={textRef}
-      font="/Russo_One.json"
-      size={textSize}
-      curveSegments={32}
-    >
-      {text}
-      <rainbowMaterial ref={materialRef} metalness={1} roughness={1} />
-    </Text3D>
+    <Center>
+      <Text3D
+        ref={textRef}
+        font="/Russo_One.json"
+        size={size.width > 1000 ? 0.5 : 0.3}
+        curveSegments={32}
+      >
+        {text}
+        <rainbowMaterial ref={materialRef} metalness={1} roughness={1} />
+      </Text3D>
+    </Center>
+
   );
 }
 
@@ -58,7 +48,7 @@ function App() {
           intensity={1}
           castShadow
         />
-        <mesh position={[0, 0 ,0]}>
+        <mesh>
           <RainbowText text="daigofujiwara.com" />
         </mesh>
         <OrbitControls target={[0, 0, 0]}/>
